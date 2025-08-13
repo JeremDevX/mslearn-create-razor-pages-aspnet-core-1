2656baf1b6c61b9293145b0c5fe6f67f38708cfd
Lavergnat Jérémie <lavergnatj@outlook.fr>
2025-08-13 18:46:22 +0200

    feat: add form to create new pizzas in PizzaList page

--- ContosoPizza/Pages/PizzaList.cshtml.cs
+++ ContosoPizza/Pages/PizzaList.cshtml.cs
@@ -8,7 +8,7 @@ namespace ContosoPizza.Pages
     public class PizzaListModel : PageModel
     {
         private readonly PizzaService _service;
-        public IList<Pizza> PizzaList { get;set; } = default!;
+        public IList<Pizza> PizzaList { get; set; } = default!;
 
         public PizzaListModel(PizzaService service)
         {
@@ -19,5 +19,25 @@ namespace ContosoPizza.Pages
         {
             PizzaList = _service.GetPizzas();
         }
+        [BindProperty]
+        public Pizza NewPizza { get; set; } = default!;
+
+        public IActionResult OnPost()
+        {
+            if (!ModelState.IsValid || NewPizza == null)
+            {
+                return Page();
+            }
+
+            _service.AddPizza(NewPizza);
+
+            return RedirectToAction("Get");
+        }
+        public IActionResult OnPostDelete(int id)
+            {
+                _service.DeletePizza(id);
+
+                return RedirectToAction("Get");
+            }
     }
 }
